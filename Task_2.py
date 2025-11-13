8a6f26e10fdd8bf9244d113d0cb1deb8f463534d
Msu global <mshafeulla@gmail.com>
2025-11-13 14:24:54 +0530

    As in the previous i took only one number ,here i made the changes to iterate it over the list of numbers

--- Task_2.py
+++ Task_2.py
@@ -7,14 +7,17 @@
 # Output Format
 # Sort the results in descending order
 
-def is_even(num):
-    if num % 2 == 0:
-        print(f"The given number {num} is a even number ")
-    else:
-        print(f"The given number {num} is a odd number ")
+def is_even(nums):
+    evens = []
+    for num in nums:
+        if num % 2 == 0:
+            print(f"The given number {num} is a even number ")
+            evens.append(num)
+        else:
+            print(f"The given number {num} is a odd number ")
+    print(f"This are the even numbers {evens}" )
 
-is_even(4)
-is_even(5)
+is_even([1,2,3,4])
 
 #As we know the logic behind if it is divisble by 2.
 #And we get a remainder as 0 then it is even
