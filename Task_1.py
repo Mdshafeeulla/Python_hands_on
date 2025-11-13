110bfb26db6f5445b15f3b8372b4715cdd637703
Msu global <mshafeulla@gmail.com>
2025-11-13 12:36:17 +0530

    inital commit

---
+++ Task_1.py
@@ -0,0 +1,11 @@
+# Exercise 1: Python Basics and Data Types
+# Write a Python program to check if a given number is prime.
+#
+# Requirements
+# Input: An integer
+# Output: True if prime, False otherwise
+#
+# Skills Tested
+# Control flow structures
+# Boolean logic
+# Algorithm efficiency
