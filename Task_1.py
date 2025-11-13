456e1039ac7c7664d532af2814dfa0818fa777a6
Msu global <mshafeulla@gmail.com>
2025-11-13 12:43:33 +0530

    Explained with code and synopsis

--- Task_1.py
+++ Task_1.py
@@ -9,3 +9,27 @@
 # Control flow structures
 # Boolean logic
 # Algorithm efficiency
+
+import math
+
+def is_prime(n: int):
+    if n < 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    limit = math.isqrt(n)
+    print(limit)
+
+    for i in range(3, limit + 1, 2):
+        if n % i == 0:
+            return False
+    print("The given number is prime")
+
+print(is_prime(4))
+
+# The logic behind this is a number is prime if it is divisible by 1 and itself
+#A for loop is used to iterate from 3 to sqrt of the number
+#We are doing the sqrt in order to avoid computation
+#As we know that sqrt of even is even and odd is odd
